877ef6ccb50aaf22294fbccb561702784f295ed3
Ip-Tec <otakhorpeter@gmail.com>
2023-11-11 06:21:43 +0000

    v2 by ip

--- errors.c
+++ errors.c
@@ -1,21 +1,24 @@
 #include "shell.h"
+
 /**
- * _eputs - prints an input string
+ *_eputs - prints an input string
  * @str: the string to be printed
  *
  * Return: Nothing
  */
 void _eputs(char *str)
 {
-        int i = 0;
-        if (!str)
-                return;
-        while (str[i] != '\0')
-        {
-                _eputchar(str[i]);
-                i++;
-        }
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
 }
+
 /**
  * _eputchar - writes the character c to stderr
  * @c: The character to print
@@ -25,17 +28,19 @@ void _eputs(char *str)
  */
 int _eputchar(char c)
 {
-        static int i;
-        static char buf[WRITE_BUF_SIZE];
-        if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-        {
-                write(2, buf, i);
-                i = 0;
-        }
-        if (c != BUF_FLUSH)
-                buf[i++] = c;
-        return (1);
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
 }
+
 /**
  * _putfd - writes the character c to given fd
  * @c: The character to print
@@ -46,19 +51,21 @@ int _eputchar(char c)
  */
 int _putfd(char c, int fd)
 {
-        static int i;
-        static char buf[WRITE_BUF_SIZE];
-        if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-        {
-                write(fd, buf, i);
-                i = 0;
-        }
-        if (c != BUF_FLUSH)
-                buf[i++] = c;
-        return (1);
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
 }
+
 /**
- * _putsfd - prints an input string
+ *_putsfd - prints an input string
  * @str: the string to be printed
  * @fd: the filedescriptor to write to
  *
@@ -66,12 +73,13 @@ int _putfd(char c, int fd)
  */
 int _putsfd(char *str, int fd)
 {
-        int i = 0;
-        if (!str)
-                return (0);
-        while (*str)
-        {
-                i += _putfd(*str++, fd);
-        }
-        return (i);
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
 }
